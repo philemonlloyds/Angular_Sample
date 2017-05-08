5bbd22d70ae35a7eab1c5b992c752aaf0bc1f524
Philemon Selvaraj <phil.lloyds@gmail.com>
2017-05-08 09:15:51 -0700

    Communication between components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ @Output() recipeWasSelected= new EventEmitter<Recipe>();
 
-  constructor() { }
+ recipes: Recipe[] = [
+   new Recipe('Sushi 1','Sushi - 1 Desc','http://maxpixel.freegreatpicture.com/static/photo/1x/Real-Sushi-Raw-Sushi-Japan-Food-207244.jpg'),
+   new Recipe('Sushi 2 ','Sushi - 2 Desc','https://c1.staticflickr.com/6/5172/5475990230_51c64e035f_b.jpg')
+   
+ ];
+ 
+ 
+ constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
